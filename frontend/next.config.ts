be14754f55ff4ca2d0b9bce3665443093bf62c2c
Roberto Darida <Roberto.darida@yahoo.com>
2026-08-20 17:09:10 +0200

    Remove standalone output

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  // Next.js PWA configuration
-  output: 'standalone',
 };
 
 export default nextConfig;
